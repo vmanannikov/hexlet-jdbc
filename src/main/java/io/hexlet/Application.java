795a008343b53e38cdcd9bb9f700de7ec5b2c769
manhetan <manhetan@gmail.com>
2024-01-20 16:15:59 +0300

    Переписано с try-with-res

--- src/main/java/io/hexlet/Application.java
+++ src/main/java/io/hexlet/Application.java
@@ -5,31 +5,25 @@ import java.sql.SQLException;
 
 public class Application {
     public static void main(String[] args) throws SQLException {
-        var conn = DriverManager.getConnection("jdbc:h2:mem:test");
+        try(var conn = DriverManager.getConnection("jdbc:h2:mem:test")){
+            var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+            try(var statement = conn.createStatement()) {
+                statement.execute(sql);
+            }
 
-        var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+            var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
+            try(var statement2 = conn.createStatement()) {
+                statement2.executeUpdate(sql2);
+            }
 
-        var statement = conn.createStatement();
-        statement.execute(sql);
-
-        statement.close();
-
-        var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-        var statement2 = conn.createStatement();
-        statement2.executeUpdate(sql2);
-        statement2.close();
-
-        var sql3 = "SELECT * FROM users";
-        var statement3 = conn.createStatement();
-        var resultSet = statement3.executeQuery(sql3);
-
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString("username"));
-            System.out.println(resultSet.getString("phone"));
+            var sql3 = "SELECT * FROM users";
+            try(var statement3 = conn.createStatement()) {
+                var resultSet = statement3.executeQuery(sql3);
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("username"));
+                    System.out.println(resultSet.getString("phone"));
+                }
+            }
         }
-
-        statement3.close();
-
-        conn.close();
     }
 }
