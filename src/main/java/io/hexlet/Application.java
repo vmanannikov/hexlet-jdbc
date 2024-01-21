f816690840ac6cc535e74597fd7889c2e4db8f23
manhetan <manhetan@gmail.com>
2024-01-21 20:03:53 +0300

    Use PreparedStatement

--- src/main/java/io/hexlet/Application.java
+++ src/main/java/io/hexlet/Application.java
@@ -2,6 +2,7 @@ package io.hexlet;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Application {
     public static void main(String[] args) throws SQLException {
@@ -11,9 +12,23 @@ public class Application {
                 statement.execute(sql);
             }
 
-            var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-            try(var statement2 = conn.createStatement()) {
-                statement2.executeUpdate(sql2);
+
+            //var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
+            var sql2 = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try(var preparedStatement = conn.prepareStatement(sql2, Statement.RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, "Tommy");
+                preparedStatement.setString(2, "123456789");
+                preparedStatement.executeUpdate();
+
+                preparedStatement.setString(1, "Jacky");
+                preparedStatement.setString(2, "33333333");
+                preparedStatement.executeUpdate();
+
+                var sql4 = "DELETE FROM users WHERE username = ?";
+                try(var statement4 = conn.prepareStatement(sql4)) {
+                    statement4.setString(1, "Jacky");
+                    statement4.executeUpdate();
+                }
             }
 
             var sql3 = "SELECT * FROM users";
